08956a91d09a5f55cc8f063196dcb0a2760af09d
Denta Muhajir <dentamuhajir@gmail.com>
2024-04-14 01:26:43 +0700

    Displaying an individual movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
+import {useParams} from "react-router-dom"
 const Movie = () => {
+
+    const [myMovie, setMyMovie] = useState({})
+
+    let id = useParams()
+    console.log(id)
+
+    useEffect( () => {
+        let movie =   {
+                id: 1,
+                title: "Highlander",
+                release_date: "1986-03-07",
+                runtime: 116,
+                mpaa_rating: "R",
+                description: "Some long description",
+        }
+        setMyMovie(movie)
+    }, [id])
+    
     return(
         <div>
-            <h2>Movie</h2>
+            <h2>Movie {myMovie.name}</h2>
             <hr/>
         </div>
     )
