dedb5237d96848e18c199daa18ee56bb4a87ce43
Denta Muhajir <dentamuhajir@gmail.com>
2024-05-10 22:44:47 +0700

    Enabling React's proxy to the back end API

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,23 +5,41 @@ import { Link } from "react-router-dom"
 const Movies = () => {
     const [movies, setMovies] = useState([])
 
-    useEffect( () => {
-        const header = new Headers()
-        header.append("Content-Type", "application/json")
+    useEffect(() => {
+        const header = new Headers();
+        header.append("Content-Type", "application/json");
         const requestOptions = {
-            method: "GET",
-            headers: header
-        }
+          method: "GET",
+          headers: header,
+        };
+      
+        fetch('http://localhost:8888/movies')
+          .then((response) => response.json())
+          .then((data) => {
+            setMovies(data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
 
-        fetch(`http://localhost:8888/movies`, requestOptions)
-                .then((response) => response.json())
-                .then((data) => {
-                    setMovies(data) 
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-    })
+    // useEffect(() => {
+    //     async function fetchMovies() {
+    //       try {
+    //         const response = await fetch('http://localhost:8888/movies');
+    //         if (!response.ok) {
+    //           throw new Error('Network response was not ok');
+    //         }
+    //         const moviesData = await response.json();
+    //         setMovies(moviesData);
+    //       } catch (error) {
+    //         console.error('Error fetching movies:', error);
+    //       }
+    //     }
+    
+    //     fetchMovies();
+    //   }, []); 
+
 
 
     // useEffect( () => {
